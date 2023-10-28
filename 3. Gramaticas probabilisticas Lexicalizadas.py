aad0113775ce6e2a5957e67dd112ed8144aaf79a
ErickArambula <eric.alejandro@hotmail.com>
2023-10-27 22:35:31 -0600

    2.0

---
+++ 3. Gramaticas probabilisticas Lexicalizadas.py
@@ -0,0 +1,23 @@
+import nltk
+from nltk.grammar import PCFG
+from nltk import ViterbiParser
+
+# Definir una gramática probabilística lexicalizada
+pcfg_grammar = PCFG.fromstring("""
+    S -> NP VP [1.0]
+    NP -> Det N [0.7] | Det N PP [0.3]
+    VP -> V [0.4] | V NP [0.4] | V NP PP [0.2]
+    PP -> P NP [1.0]
+    Det -> 'the' [1.0]
+    N -> 'cat' [0.4] | 'dog' [0.6]
+    V -> 'chased' [0.7] | 'ate' [0.3]
+    P -> 'with' [1.0]
+    """)
+
+# Crear un analizador Viterbi para la gramática
+parser = ViterbiParser(pcfg_grammar)
+
+# Analizar una oración
+sentence = "the cat chased the dog with the bone".split()
+for tree in parser.parse(sentence):
+    tree.pretty_print()
